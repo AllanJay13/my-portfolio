d451bfc6f78b59dabb78154d530ce5fa5ffd033a
AllanJay13 <allanjaysarino@gmail.com>
2023-05-22 21:45:31 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Gif from './components/Gif';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Experience from './components/Experience';
+import Cards from './components/Cards';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Gif/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Cards/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Gif.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function Gif() {
+  return (
+    <section id="gf">
+        <div className='gif'>
+            <img src="./images/gifs.gif" alt='gif'/>
+        </div>
+    </section>
+  )
+}
+
+export default Gif
